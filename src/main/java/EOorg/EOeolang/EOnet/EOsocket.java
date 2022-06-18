ed8bcc4c07abc2e916b2dcdd47b58995bc1aed3b
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-06-18 09:05:22 +0500

    Create EOsocket.java

---
+++ src/main/java/EOorg/EOeolang/EOnet/EOsocket.java
@@ -0,0 +1,102 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 kerelape
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+// @checkstyle PackageNameCheck (1 line)
+package EOorg.EOeolang.EOnet;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import org.eolang.AtComposite;
+import org.eolang.AtFree;
+import org.eolang.Data;
+import org.eolang.Param;
+import org.eolang.PhDefault;
+import org.eolang.Phi;
+
+/**
+ * Socket.
+ *
+ * @since 0.0.0
+ * @checkstyle ClassDataAbstractionCouplingCheck (100 lines)
+ */
+public final class EOsocket extends PhDefault {
+
+    /**
+     * Ctor.
+     *
+     * @param sigma Parent.
+     */
+    public EOsocket(final Phi sigma) {
+        super(sigma);
+        this.add("address", new AtFree());
+        this.add("port", new AtFree());
+        this.add(
+            "φ",
+            new AtComposite(
+                this,
+                rho -> new Data.ToPhi(
+                    new StringBuilder()
+                        .append(new Param(rho, "address").strong(String.class))
+                        .append(":")
+                        .append(new Param(rho, "port").strong(Long.class).toString())
+                        .toString()
+                )
+            )
+        );
+        this.add(
+            "connect",
+            new AtComposite(
+                this,
+                rho -> {
+                    final Socket connection = new Socket();
+                    connection.connect(address(rho));
+                    return new EOconnection(rho, connection);
+                }
+            )
+        );
+        this.add(
+            "listen",
+            new AtComposite(
+                this,
+                rho -> {
+                    final ServerSocket server = new ServerSocket();
+                    server.bind(address(rho));
+                    return new EOserver_connection(rho, server);
+                }
+            )
+        );
+    }
+
+    // @checkstyle JavadocMethodCheck (1 line)
+    private static InetSocketAddress address(final Phi socket) throws UnknownHostException {
+        return new InetSocketAddress(
+            InetAddress.getByName(
+                new Param(socket, "address").strong(String.class)
+            ),
+            new Param(socket, "port").strong(Long.class).intValue()
+        );
+    }
+}
