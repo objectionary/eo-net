78a80b40ee93305cbfef36dd7dae9ae8e7da187f
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-06-19 05:12:14 +0500

    Create EOsocketTest.java

---
+++ src/test/java/EOorg/EOeolang/EOnet/EOsocketTest.java
@@ -0,0 +1,89 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 kerelape
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package EOorg.EOeolang.EOnet;
+
+import java.util.Random;
+import org.eolang.Data;
+import org.eolang.Dataized;
+import org.eolang.PhWith;
+import org.eolang.Phi;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+public final class EOsocketTest {
+
+    @Test
+    public void echo() {
+        final Phi socket = new PhWith(
+            new PhWith(
+                new EOsocket(Phi.Φ),
+                "address",
+                new Data.ToPhi("localhost")
+            ),
+            "port",
+            new Data.ToPhi(8080L)
+        );
+        new Thread(() -> {
+            final Phi connection = socket
+                .attr("listen").get()
+                .attr("accept").get();
+            new Dataized(
+                new PhWith(
+                    connection
+                        .attr("as-output").get()
+                        .attr("write").get(),
+                    "data",
+                    connection
+                        .attr("as-input").get()
+                        .attr("read").get()
+                )
+            ).take();
+        }).start();
+        final long data = (byte) new Random().nextLong();
+        final Phi connection = socket.attr("connect").get();
+        new Dataized(
+            new PhWith(
+                connection
+                    .attr("as-output").get()
+                    .attr("write").get(),
+                "data",
+                new Data.ToPhi((long)(byte)data)
+            )
+        ).take();
+        final long response = new Dataized(
+            connection
+                .attr("as-input").get()
+                .attr("read").get()
+        ).take(Long.class);
+        new Dataized(
+            connection.attr("close").get()
+        ).take();
+        MatcherAssert.assertThat(
+            response,
+            Matchers.equalTo(data)
+        );
+    }
+}
