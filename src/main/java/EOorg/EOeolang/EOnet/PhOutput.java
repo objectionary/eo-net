2c64aa61c6f2b574510f61e823592296c61b57d0
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-06-24 09:04:55 +0500

    Ignore violation

--- src/main/java/EOorg/EOeolang/EOnet/PhOutput.java
+++ src/main/java/EOorg/EOeolang/EOnet/PhOutput.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
+// @checkstyle PackageNameCheck (1 line)
 package EOorg.EOeolang.EOnet;
 
 import java.io.OutputStream;
